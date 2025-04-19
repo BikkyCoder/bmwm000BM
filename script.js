f38989b09f029cd39ffd1d30d807dd177ea3b3f3
BikkyCoder <bikkysabb@gmail.com>
2025-04-19 17:48:02 +0530

    new change

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-0Y5GVS3CXG');
+
 const wrappers = document.querySelectorAll(".image-wrapper");
 
 wrappers.forEach(wrapper => {
